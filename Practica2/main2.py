153c2870cde2d199b913b7652a6a3c53c46cfec7
JotaGalera <javigalera94@gmail.com>
2018-10-02 17:02:51 +0200

    Practica 2 terminada

--- Practica2/main2.py
+++ Practica2/main2.py
@@ -5,6 +5,16 @@ app = Flask(__name__)
 @app.route('/user/<username>')
 def perfilUser(username):
     return 'Usuario %s' % username
+#http://0.0.0.0:5000/user/CualquierNombre
+
+
+@app.route('/user/')
+def perfilUserConParametros(username=None):
+    parametro1=request.args.get('variable1')
+    parametro2=request.args.get('variable2')
+    return parametro1 + ' ' + parametro2
+#http://0.0.0.0:5000/user/?variable1=hole&variable2=adios
+
 
 @app.errorhandler(404)
 def pagenotfound(error):
@@ -12,3 +22,4 @@ def pagenotfound(error):
 
 if __name__ == "__main__":
   app.run(debug = True, host='0.0.0.0')
+#http://0.0.0.0:5000/CualquierOtraRuta
